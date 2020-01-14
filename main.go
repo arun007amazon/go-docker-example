813fd7a2859c5cd689f97c132349c120e50d0326
arun007amazon <50901047+arun007amazon@users.noreply.github.com>
2020-01-14 14:23:51 +1100

    Update main.go

--- main.go
+++ main.go
@@ -14,6 +14,7 @@ import (
         "github.com/gorilla/mux"
         "gopkg.in/natefinch/lumberjack.v2"
 	"github.com/nats-io/nats.go"
+)
 
 var natsServer = flag.String("natsServer", "nats://operator-nats:4222", "Comma seperated nats servers")
 
